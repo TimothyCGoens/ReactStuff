378747d003a61cdfd47076c1aa2fa806f54694ca
Tim Goens <timcgoens@gmail.com>
2020-07-22 15:23:58 -0500

    working on the hooks section

---
+++ widgets/src/components/App.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+// import Accordion from "./Accordion";
+// import Search from "./Search";
+import Dropdown from "./Dropdown";
+
+// const items = [
+//   {
+//     title: "What is React?",
+//     content: "React is a front end javascript framework",
+//   },
+//   {
+//     title: "Why use React?",
+//     content: "React is a favorite library among engineers.",
+//   },
+//   {
+//     title: "How do you use React?",
+//     content: "You use React by creating components",
+//   },
+// ];
+
+const options = [
+  {
+    label: "The Color Red",
+    value: "red",
+  },
+  {
+    label: "The Color Green",
+    value: "green",
+  },
+  {
+    label: "A Shade of Blue",
+    value: "blue",
+  },
+];
+
+export default () => {
+  const [selected, setSelected] = useState(options[0]);
+  return (
+    <div>
+      <Dropdown
+        selected={selected}
+        onSelectedChange={setSelected}
+        options={options}
+      />
+    </div>
+  );
+};
