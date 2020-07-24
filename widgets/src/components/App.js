9a727f6eddfb8f5b9d671d666e869f4b61eb7806
Tim Goens <timcgoens@gmail.com>
2020-07-24 10:16:39 -0500

    finished the widget app

--- widgets/src/components/App.js
+++ widgets/src/components/App.js
@@ -1,45 +1,68 @@
 import React, { useState } from "react";
-// import Accordion from "./Accordion";
-// import Search from "./Search";
-// import Dropdown from "./Dropdown";
+import Accordion from "./Accordion";
+import Search from "./Search";
+import Dropdown from "./Dropdown";
 import Translate from "./Translate";
+import Route from "./Route";
+import Header from "./Header";
 
-// const items = [
-//   {
-//     title: "What is React?",
-//     content: "React is a front end javascript framework",
-//   },
-//   {
-//     title: "Why use React?",
-//     content: "React is a favorite library among engineers.",
-//   },
-//   {
-//     title: "How do you use React?",
-//     content: "You use React by creating components",
-//   },
-// ];
+const items = [
+  {
+    title: "What is React?",
+    content: "React is a front end javascript framework",
+  },
+  {
+    title: "Why use React?",
+    content: "React is a favorite library among engineers.",
+  },
+  {
+    title: "How do you use React?",
+    content: "You use React by creating components",
+  },
+];
 
-// const options = [
-//   {
-//     label: "The Color Red",
-//     value: "red",
-//   },
-//   {
-//     label: "The Color Green",
-//     value: "green",
-//   },
-//   {
-//     label: "A Shade of Blue",
-//     value: "blue",
-//   },
-// ];
+const options = [
+  {
+    label: "The Color Red",
+    value: "red",
+  },
+  {
+    label: "The Color Green",
+    value: "green",
+  },
+  {
+    label: "A Shade of Blue",
+    value: "blue",
+  },
+];
+
+const showComponent = (route, component) => {
+  return window.location.pathname === route ? component : null;
+};
 
 export default () => {
-  // const [selected, setSelected] = useState(options[0]);
+  const [selected, setSelected] = useState(options[0]);
 
   return (
     <div>
-      <Translate />
+      <Header />
+      <Route path="/">
+        <Accordion items={items} />
+      </Route>
+      <Route path="/list">
+        <Search />
+      </Route>
+      <Route path="/dropdown">
+        <Dropdown
+          label="Select a color"
+          options={options}
+          selected={selected}
+          onSelectedChange={setSelected}
+        />
+      </Route>
+      <Route path="/translate">
+        <Translate />
+      </Route>
     </div>
   );
 };
